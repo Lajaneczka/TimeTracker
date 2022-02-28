15853a3903bde3ea70df3a48373d6776493adf76
Lajaneczka <aleks.malecka1990@gmail.com>
2022-02-28 22:29:00 +0100

    timer in main component has been added, also  Readme file has bee created

---
+++ Readme.md
@@ -0,0 +1,10 @@
+In this app, a user can add, delete and modify various timers.
+Each timer corresponds to a different task that the user would like to keep time for.
+
+Open your terminal/cmd and run the following commandes:
+
+type git clone https://github.com/Lajaneczka/TimeTracker.git
+cd time-tracker to enter to app folder.
+yarn to install the app dependencies.
+expo start to run the aplication.
+w  to view the app the browser.
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  FlatList,
+} from "react-native";
+import { TaskTimer } from "./TaskTimer";
 
-export const Task = (props) => {
+export const Task = ({ text, timeDuration }) => {
+  // const [selectedValue, setSelectedValue] = useState("java");
+
   return (
     <View style={style.item}>
       <View style={style.itemLeft}>
         <View style={style.square}></View>
-        <Text style={style.itemText}>{props.text}</Text>
+        <Text style={style.itemText}>{text}</Text>
+
+        <TaskTimer timeDuration={timeDuration} />
+   
       </View>
       <View style={style.circular}></View>
     </View>
@@ -14,36 +26,36 @@ export const Task = (props) => {
 };
 
 const style = StyleSheet.create({
-    item: {
-        backgroundColor: '#fff',
-        padding: 15,
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 20
-    },
-    itemLeft: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        flexWrap: 'wrap'
-    },
-    square: {
-        width: 24,
-        height: 24,
-        backgroundColor: '#4db8ff',
-        opacity: 0.4,
-        borderRadius: 5,
-        marginRight: 15,
-    },
-    itemText: {
-        maxWidth: '80%'
-    },
-    circular: {
-        width: 12,
-        height: 12,
-        borderColor: '#4db8ff',
-        borderWidth: 2,
-        borderRadius: 5,
-    }
+  item: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  // square: {
+  //     width: 24,
+  //     height: 24,
+  //     backgroundColor: '#4db8ff',
+  //     opacity: 0.4,
+  //     borderRadius: 5,
+  //     marginRight: 15,
+  // },
+  itemText: {
+    maxWidth: "80%",
+  },
+  // circular: {
+  //     width: 12,
+  //     height: 12,
+  //     borderColor: '#4db8ff',
+  //     borderWidth: 2,
+  //     borderRadius: 5,
+  // }
 });
